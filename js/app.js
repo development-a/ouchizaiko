7cc9d6227da6061c3d06bc813bb8b5de2f981a03
development-a <chari.rock.1122@gmail.com>
2026-06-19 19:46:08 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,216 +1,36 @@
-document.addEventListener(
-"DOMContentLoaded",
-init
-);
-
-
-
-let pressTimer;
-
-
-
-function init()
-{
-
-document
-.getElementById("add-button")
-.addEventListener(
-"click",
-addItemForm
-);
-
-
-document
-.getElementById("filter-category")
-.addEventListener(
-"change",
-render
-);
-
-
-render();
-
-}
-
-
-
-
-
-function render()
-{
-
-renderSelect();
-
-renderInventory();
-
-updateCount();
-
-}
-
-
-
-
-
-
-
-function addItemForm()
-{
-
-let name =
-document
-.getElementById("item-name")
-.value;
-
-
-let category =
-document
-.getElementById("item-category")
-.value;
-
-
-
-if(!name)
-return;
-
-
-
-addItem(
-{
-name,
-category,
-stock:true
-}
-);
-
-
-
-document
-.getElementById("item-name")
-.value="";
-
-
-render();
-
-}
-
-
-
-
-
-
-
-function renderSelect()
-{
-
-let categories =
-getCategories();
-
-
-let select =
-document
-.getElementById("item-category");
-
-
-
-select.innerHTML="";
-
-
-
-categories.forEach(
-c =>
-{
-
-select.innerHTML +=
-`
-<option value="${c}">
-${c}
-</option>
-`;
-
-});
-
-}
-
-
-
-
-
-
-
-
-
-function renderInventory()
-{
-
-let area =
-document
-.getElementById("inventory-list");
-
-
-
-area.innerHTML="";
-
-
-
-let items =
-getItems();
-
-
-
-let filter =
-document
-.getElementById("filter-category")
-.value;
-
-
-
-
-if(filter!=="all")
-{
-
-items =
-items.filter(
-x =>
-x.category===filter
-);
-
-}
-
-
-
-
-
-
-
-items.forEach(
-item =>
-{
-
-let div =
+const div =
 document.createElement("div");
 
 
 
 div.className =
-item.stock
-?
-"item"
-:
-"item no-stock";
+"item-wrapper";
 
 
 
 
 div.innerHTML =
 `
+
+<div class="swipe-delete">
+
+×
+</div>
+
+
+<div class="item ${item.stock ? "" : "no-stock"}">
+
+
 <div class="watermark">
 
-在庫なし
+${item.stock ? "" : "在庫なし"}
 
 </div>
 
 
+
 <div class="item-info">
+
 
 <span class="item-name">
 
@@ -228,7 +48,123 @@ ${item.category}
 
 </div>
 
+
+
+</div>
+
 `;
+
+
+
+
+
+const card =
+div.querySelector(".item");
+
+
+
+let startX=0;
+
+
+
+card.addEventListener(
+"touchstart",
+e =>
+{
+
+startX =
+e.touches[0].clientX;
+
+});
+
+
+
+
+card.addEventListener(
+"touchend",
+e =>
+{
+
+let endX =
+e.changedTouches[0].clientX;
+
+
+
+if(startX-endX > 50)
+{
+
+card.classList.add(
+"swiped"
+);
+
+}
+
+
+if(endX-startX > 50)
+{
+
+card.classList.remove(
+"swiped"
+);
+
+}
+
+});
+
+
+
+
+
+
+div.querySelector(
+".swipe-delete"
+)
+.onclick =
+() =>
+{
+
+if(confirm("削除しますか？"))
+{
+
+deleteItem(item.id);
+
+render();
+
+}
+
+};
+
+
+
+
+
+
+
+card.onclick =
+() =>
+{
+
+if(
+card.classList.contains("swiped")
+)
+{
+
+card.classList.remove(
+"swiped"
+);
+
+return;
+
+}
+
+
+
+toggleStock(item.id);
+
+};
+
+
+
 
 
 
@@ -238,7 +174,7 @@ let timer;
 
 
 
-div.addEventListener(
+card.addEventListener(
 "mousedown",
 () =>
 {
@@ -254,7 +190,7 @@ openMenu(item.id),
 
 
 
-div.addEventListener(
+card.addEventListener(
 "mouseup",
 () =>
 clearTimeout(timer)
@@ -263,149 +199,5 @@ clearTimeout(timer)
 
 
 
-div.onclick =
-() =>
-toggleStock(item.id);
-
-
-
 
 area.appendChild(div);
-
-
-});
-
-
-}
-
-
-
-
-
-
-
-function openMenu(id)
-{
-
-let item =
-getItems()
-.find(
-x=>x.id===id
-);
-
-
-
-let action =
-prompt(
-"操作を選択\n1:編集\n2:削除"
-);
-
-
-
-if(action==="1")
-{
-
-let name =
-prompt(
-"商品名",
-item.name
-);
-
-
-
-if(name)
-{
-
-item.name=name;
-
-updateItem(item);
-
-render();
-
-}
-
-}
-
-
-
-
-if(action==="2")
-{
-
-deleteItem(id);
-
-render();
-
-}
-
-
-}
-
-
-
-
-
-
-
-function toggleStock(id)
-{
-
-let items =
-getItems();
-
-
-let item =
-items.find(
-x=>x.id===id
-);
-
-
-
-item.stock =
-!item.stock;
-
-
-
-updateItem(item);
-
-
-render();
-
-}
-
-
-
-
-
-
-function updateCount()
-{
-
-let items =
-getItems();
-
-
-
-document
-.getElementById("total-items")
-.textContent =
-`登録 ${items.length}件`;
-
-
-
-
-
-let count =
-items.filter(
-x =>
-!x.stock
-).length;
-
-
-
-document
-.getElementById("shopping-count")
-.textContent =
-`買い物 ${count}件`;
-
-}
